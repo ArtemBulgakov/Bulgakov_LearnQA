9de3bf9c820fd0271dfd4cf1207600a2fc55e544
bulgakov-av <bulgakov-av@ural.rt.ru>
2023-05-08 17:17:42 +0500

    api/get_text

--- src/test/java/HelloWordTest.java
+++ src/test/java/HelloWordTest.java
@@ -1,9 +1,14 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWordTest {
 
     @Test
     public void testHellow(){
-        System.out.println("Hello from Artem");
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
     }
 }
