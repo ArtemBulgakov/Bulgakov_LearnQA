478527954deaac77282a66dc7b77c28a8a5ea0d5
bulgakov-av <bulgakov-av@ural.rt.ru>
2023-05-18 00:56:24 +0500

    Ex8: Токены - отправка нескольких запросов через задержку

--- src/test/java/HelloWordTest.java
+++ src/test/java/HelloWordTest.java
@@ -6,35 +6,44 @@ public class HelloWordTest {
 
 
     @Test
-    public void testRestAssured() {
+    public void testRestAssured() throws InterruptedException {
 
-        int i = 1;
-        Response response = RestAssured
+        Response response1 = RestAssured
                 .given()
-                .redirects()
-                .follow(false)
                 .when()
-                .get("https://playground.learnqa.ru/api/long_redirect")
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
                 .andReturn();
-
-        String HedLocation = response.getHeader("Location");
-        System.out.println(i + " " + "redirect to - " + response.getHeader("Location"));
+        response1.print();
         System.out.println();
 
-        while (response.getStatusCode() > 200) {
-            response = RestAssured
-                    .given()
-                    .redirects()
-                    .follow(false)
-                    .when()
-                    .get(HedLocation)
-                    .andReturn();
-            if (response.getStatusCode() > 200) {
-                i++;
-                System.out.println(i + " " + "redirect to - " + response.getHeader("Location"));
-                System.out.println();
-            }
-            HedLocation = response.getHeader("Location");
+
+        Response response2 = RestAssured
+                .given()
+                .queryParam("token", response1.jsonPath().getString("token"))
+                .when()
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                .andReturn();
+        response2.print();
+        System.out.println();
+
+
+        if (response2.jsonPath().getString("status") != "Job is ready") {
+            Thread.sleep(response1.jsonPath().getInt("seconds") * 1000);
+        }
+        Response response3 = RestAssured
+                .given()
+                .queryParam("token", response1.jsonPath().getString("token"))
+                .when()
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                .andReturn();
+        response3.print();
+
+
+        if ((response3.jsonPath().getString("result") != null) &&
+                (response3.jsonPath().getString("status").equals("Job is ready"))) {
+            System.out.println();
+            System.out.println("It's good");
         }
     }
+
 }
