3f31b4ab259066a1f65aa3dc24ecbc8eda81e54a
bulgakov-av <bulgakov-av@ural.rt.ru>
2023-05-18 17:01:57 +0500

    Ex11: Тест запроса на метод cookie

--- src/test/java/Lesson3.java
+++ src/test/java/Lesson3.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import io.restassured.internal.common.assertion.Assertion;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -12,7 +13,19 @@ public class Lesson3 {
         int length = 15; // минимальное количество символов, которое должно быть в строке, не включая это число
         Assertions.assertTrue(data.length() > length, "the length of the string is longer - " + length);
         System.out.println("the length of the string is longer - " + length);
+    }
 
+    @Test
+    public void task2_Ex11() {
+
+        Response response = RestAssured
+                .given()
+                .when()
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+
+        Assertions.assertNotNull(response.getCookie("HomeWork"));
+        Assertions.assertEquals("hw_value",response.getCookie("HomeWork"));
     }
 
 }
